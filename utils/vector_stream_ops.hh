d91f1b8f5d0fc0a7363c74ed81dbeef2a3a70a15
Samuel Pomeroy <1690@holbertonschool.com>
2022-09-11 05:10:22 +0000

    utils added vector stream operators

---
+++ utils/vector_stream_ops.hh
@@ -0,0 +1,133 @@
+#ifndef LC_VECTOR_STREAM_OPS
+#define LC_VECTOR_STREAM_OPS
+
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <utility>  // move
+
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+    os << '[';
+    for (auto it {v.begin()}, last {--v.end()}; it != v.end(); ++it) {
+        os << *it;
+        if (it != last)
+            os << ',';
+    }
+    os << ']';
+    return os;
+}
+
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os,
+                         const std::vector<std::vector<T>> &v) {
+    os << '[';
+    for (auto it {v.begin()}, last {--v.end()}; it != v.end(); ++it) {
+        os << *it;
+        if (it != last)
+            os << ',';
+    }
+    os << ']';
+    return os;
+}
+
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os,
+                         const std::vector<std::vector<std::vector<T>>> &v) {
+    os << '[';
+    for (auto it {v.begin()}, last {--v.end()}; it != v.end(); ++it) {
+        os << *it;
+        if (it != last)
+            os << ',';
+    }
+    os << ']';
+    return os;
+}
+
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+    is >> std::ws;  // ensure leading whitespace skipped
+    std::vector<T> new_v;
+    if (is.peek() != '[')
+        throw std::invalid_argument("expected '[' at start of vector");
+    is.get();  // consume '['
+    T temp;
+    for (char peek(is.peek()); is.good() && peek != ']';) {
+        is >> std::ws;
+        is >> temp;
+        if (!is.good() && !is.eof())
+            throw std::invalid_argument("could not read element from stream");
+        new_v.emplace_back(temp);
+        is >> std::ws;
+        peek = is.peek();
+        if (is.eof() || (peek != ',' && peek != ']'))
+            throw std::invalid_argument("expected ',' or ']' after element");
+        if (peek == ',')
+            is.get();
+    }
+    is.get();
+    v = std::move(new_v);
+    return is;
+}
+
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<std::vector<T>> &v) {
+    is >> std::ws;  // ensure leading whitespace skipped
+    std::vector<std::vector<T>> new_v;
+    if (is.peek() != '[')
+        throw std::invalid_argument("expected '[' at start of vector");
+    is.get();  // consume '['
+    std::vector<T> temp;
+    for (char peek(is.peek()); is.good() && peek != ']';) {
+        is >> std::ws;
+        is >> temp;
+        if (!is.good() && !is.eof())
+            throw std::invalid_argument("could not read element from stream");
+        new_v.emplace_back(temp);
+        is >> std::ws;
+        peek = is.peek();
+        if (is.eof() || (peek != ',' && peek != ']'))
+            throw std::invalid_argument("expected ',' or ']' after element");
+        if (peek == ',')
+            is.get();
+    }
+    is.get();
+    v = std::move(new_v);
+    return is;
+}
+
+
+template<typename T>
+std::istream &operator>>(std::istream &is, std::vector<std::vector<std::vector<T>>> &v) {
+    is >> std::ws;  // ensure leading whitespace skipped
+    std::vector<std::vector<std::vector<T>>> new_v;
+    if (is.peek() != '[')
+        throw std::invalid_argument("expected '[' at start of vector");
+    is.get();  // consume '['
+    std::vector<std::vector<T>> temp;
+    for (char peek(is.peek()); is.good() && peek != ']';) {
+        is >> std::ws;
+        is >> temp;
+        if (!is.good() && !is.eof())
+            throw std::invalid_argument("could not read element from stream");
+        new_v.emplace_back(temp);
+        is >> std::ws;
+        peek = is.peek();
+        if (is.eof() || (peek != ',' && peek != ']'))
+            throw std::invalid_argument("expected ',' or ']' after element");
+        if (peek == ',')
+            is.get();
+    }
+    is.get();
+    v = std::move(new_v);
+    return is;
+}
+
+
+#endif  // LC_VECTOR_STREAM_OPS
